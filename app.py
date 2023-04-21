fcfd1b2a9b32c510297cae53437fee33f48623b3
Zhigang Wei <zhiw803@gmail.com>
2023-04-21 23:45:19 +0000

    set docker

--- app.py
+++ app.py
@@ -46,4 +46,4 @@ def result(filename):
     return render_template("result.html", filename=filename, labels=labels)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(port=8080, debug=True)
